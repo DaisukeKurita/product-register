9895dbc7e451b9a74988266b0e1a8b69745bb2e3
栗田大輔 <daikons.msn36@gmail.com>
2022-09-11 19:19:46 +0900

    first commit

---
+++ app/views/produces/_produce.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! produce, :id, :name, :price, :vendor, :created_at, :updated_at
+json.url produce_url(produce, format: :json)
---
+++ db/migrate/20220911092427_create_produces.rb
@@ -0,0 +1,11 @@
+class CreateProduces < ActiveRecord::Migration[5.2]
+  def change
+    create_table :produces do |t|
+      t.string :name
+      t.integer :price
+      t.string :vendor
+
+      t.timestamps
+    end
+  end
+end
